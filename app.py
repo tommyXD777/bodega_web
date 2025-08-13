ed0c5de0fa4bb567a6f3a22628d34ef9b91d6e60
tommyXD777 <yessidarias133@gmail.com>
2025-08-13 14:52:19 -0500

    00

--- app.py
+++ app.py
@@ -770,5 +770,5 @@ def add_credit_payment():
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if _name_ == '_main_':
+    app.run(debug=True, host="0.0.0.0")
